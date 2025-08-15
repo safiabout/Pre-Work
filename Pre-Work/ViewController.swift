3377ff9587686eb8030e43bcfa93d0bd3f1fed76
safiabout <safiaboutalebab@gmail.com>
2025-08-14 17:43:41 -0700

    add initial files

--- Pre-Work/ViewController.swift
+++ Pre-Work/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myName: UILabel!
+    @IBOutlet weak var mySchool: UILabel!
+    @IBOutlet weak var myAspirationalJob: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        myName.textColor = .black
+        mySchool.textColor = .black
+        myAspirationalJob.textColor = .black
+    }
+    
+    func changeColor() -> UIColor {
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
     }
 
 
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+    
+    @IBAction func changeFontColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        
+        myName.textColor = randomColor
+        mySchool.textColor = randomColor
+        myAspirationalJob.textColor = randomColor
+    }
 }
 
